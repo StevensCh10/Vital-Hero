86eef00384d2f145daf667aad99d86725c8a59df
StevensCh10 <stevensch10@outlook.com>
2024-01-25 18:25:29 -0300

    Alteração nas classes de serviço "BloodCenterService" e "DoctorService": Assinaturas dos métodos criados e injeção das dependencias necessárias adicionadas.

--- vitalhero/src/main/java/com/vitalhero/fullstack/service/BloodCenterService.java
+++ vitalhero/src/main/java/com/vitalhero/fullstack/service/BloodCenterService.java
@@ -1,14 +1,43 @@
 package com.vitalhero.fullstack.service;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.vitalhero.fullstack.model.BloodCenter;
+import com.vitalhero.fullstack.model.BloodStock;
+import com.vitalhero.fullstack.model.Scheduling;
 import com.vitalhero.fullstack.repository.BloodCenterRepository;
+import com.vitalhero.fullstack.repository.BloodStockRepository;
+import com.vitalhero.fullstack.repository.SchedulingRepository;
 
 @Service
 public class BloodCenterService {
     
     @Autowired
-    private BloodCenterRepository repository;
+    private BloodCenterRepository bcRepository;
+
+    @Autowired
+    private SchedulingRepository schedulingRepo;
+
+    @Autowired
+    private BloodStockRepository bsRepo;
+
+    public BloodCenter find(Long bcID){
+        return null;
+    }
+
+    public ArrayList<Scheduling> schedulings(Long bcID){
+        return null;
+    }
+
+    public Scheduling specifScheduling(Long schedulingID){
+        return null;
+    }
+
+    public BloodStock bloodStock(Long bcID){
+        return null;
+    }
 
 }
--- vitalhero/src/main/java/com/vitalhero/fullstack/service/DoctorService.java
+++ vitalhero/src/main/java/com/vitalhero/fullstack/service/DoctorService.java
@@ -3,12 +3,26 @@ package com.vitalhero.fullstack.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.vitalhero.fullstack.model.Doctor;
+import com.vitalhero.fullstack.model.Screening;
 import com.vitalhero.fullstack.repository.DoctorRepository;
+import com.vitalhero.fullstack.repository.ScreeningRepository;
 
 @Service
 public class DoctorService {
     
     @Autowired
-    private DoctorRepository repository;
+    private DoctorRepository doctorRepo;
+
+    @Autowired
+    private ScreeningRepository screeningRepo;
+
+    public Doctor find(Long doctorID){
+        return null;
+    }
+
+    public Screening validatedScreening(Long doctorID){
+        return null;
+    }
 
 }
