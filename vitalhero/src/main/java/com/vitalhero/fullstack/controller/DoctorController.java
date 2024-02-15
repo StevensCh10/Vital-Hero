0223a25f695832806910c1d3b2bd69ffa6b53407
StevensCh10 <stevensch10@outlook.com>
2024-02-15 16:07:30 -0300

    - Injeção de dependência das classes de serviço necessárias na classe DoctorController. - Criação dos endpoints

--- vitalhero/src/main/java/com/vitalhero/fullstack/controller/DoctorController.java
+++ vitalhero/src/main/java/com/vitalhero/fullstack/controller/DoctorController.java
@@ -1,19 +1,49 @@
 package com.vitalhero.fullstack.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.vitalhero.fullstack.model.Doctor;
+import com.vitalhero.fullstack.model.Screening;
 import com.vitalhero.fullstack.service.DoctorService;
+import com.vitalhero.fullstack.service.ScreeningService;
 
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/doctor")
 public class DoctorController {
 
-    private final DoctorService service;
+    private final DoctorService doctorService;
+    private final ScreeningService screeningService;
 
-    public DoctorController(DoctorService service){
-        this.service = service;
+    public DoctorController(DoctorService doctorService, ScreeningService screeningService){
+        this.doctorService = doctorService;
+        this.screeningService = screeningService;
     }
     
+    @PostMapping()
+    public Doctor addDoctor(@RequestBody Doctor newDoctor){
+        return doctorService.register(newDoctor);
+    }
+
+    @GetMapping("/{docID}")
+    public Doctor find(@PathVariable Long docID){
+        return doctorService.find(docID);
+    }
+
+    @DeleteMapping("/{docID}")
+    public void deleteDoctor(@PathVariable Long docID){
+        doctorService.deleteDoctor(docID);
+    }
+
+    @PutMapping("/validatescreening/{docID}")
+    public Screening validateScreening(@PathVariable Long screeningID, @PathVariable Long docID){
+        return screeningService.validatedScreening(docID);
+    }
 }
