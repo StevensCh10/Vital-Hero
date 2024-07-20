3501ee211a6f73e50c674ff757fc9820d488e924
StevensCh10 <stevensch10@outlook.com>
2024-07-19 22:48:18 -0300

    Autorizando URL para acessar Documentação da API.

--- Backend/src/main/java/com/vitalhero/fullstack/config/SecurityConfig.java
+++ Backend/src/main/java/com/vitalhero/fullstack/config/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/**").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
